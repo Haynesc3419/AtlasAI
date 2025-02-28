8f975a41922983b5e98cdc2a2def291cb0599e85
Christian Haynes <Haynesc3419@gmail.com>
2025-02-28 14:39:28 -0500

    security keys

--- config.js
+++
@@ -1,6 +0,0 @@
-export default {
-    GEMINI_API_KEY:"AIzaSyBgIuHXP-Qe7PnBuE1KxDr3HqB3ldh0xKs",
-    GEMINI_API_MODEL:"gemini-1.5-flash-001",
-    DEEPSEEK_API_KEY: "sk-dd74b8a0a42a48c78ec5af414dff864b",
-    DEEPSEEK_BASE_URL: "https://api.deepseek.com/v1"
-};
